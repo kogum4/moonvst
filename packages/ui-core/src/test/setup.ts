c9ce2d900c220845172366a322702955e50e303d
kogum4 <koguma2102@gmail.com>
2026-02-24 00:14:29 +0900

    test(ui): cleanup DOM after each test to prevent timeout flake

--- packages/ui-core/src/test/setup.ts
+++ packages/ui-core/src/test/setup.ts
@@ -1,6 +1,11 @@
 import '@testing-library/jest-dom/vitest'
-import { beforeEach } from 'vitest'
+import { cleanup } from '@testing-library/react'
+import { afterEach, beforeEach } from 'vitest'
 
 beforeEach(() => {
   window.localStorage.clear()
+})
+
+afterEach(() => {
+  cleanup()
 })
